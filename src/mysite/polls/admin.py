82e5acd6316d0dc9c3342c5280166902e0dab64c
pietrog <pietro.gastaldo@gmail.com>
2023-10-05 18:08:51 +0200

    Optimize choice registration

--- src/mysite/polls/admin.py
+++ src/mysite/polls/admin.py
@@ -4,11 +4,17 @@ from .models import Choice, Question
 # Register your models here.
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 
-admin.site.register(Choice, Question, QuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
